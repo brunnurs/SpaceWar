77d0b8984577f052a26f462027c97e64dd380a73
Ursin <ursin.brunner@drallo.ch>
2015-01-16 11:45:03 +0100

    first commit. There is one planet which grows over time and changes it's size

---
+++ Assets/_Scripts/GameController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public float amountOfPlanets;
+    public Vector3 spawnRange;
+    public GameObject planet;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        Vector3 spawnPosition = new Vector3 (Random.Range (-spawnRange.x, spawnRange.x), Random.Range (-spawnRange.y, spawnRange.y), 0);
+        Quaternion spawnRotation = Quaternion.identity;
+        Instantiate (planet, spawnPosition, spawnRotation);
+    }
+
+    void SpawnPlanet()
+    {
+        Vector3 spawnPosition = new Vector3 (Random.Range (-spawnRange.x, spawnRange.x), Random.Range (-spawnRange.y, spawnRange.y), 0);
+        Quaternion spawnRotation = Quaternion.identity;
+        GameObject newPlanet = Instantiate (planet, spawnPosition, spawnRotation) as GameObject;
+
+        SetPlanetSizeRandomly();
+        GrowthController growthController = newPlanet.GetComponent<GrowthController>();
+        growthController.ShipCounter = Random.Range(GrowthController.MIN_SHIPS, GrowthController.MAX_SHIPS);
+    }
+
+    
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+}
---
+++ Assets/_Scripts/GrowthController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GrowthController : MonoBehaviour
+{
+    /// <summary>
+    /// All planets should have at least 10 ships on it, and max 100. The size grows from 1 to 3 
+    /// </summary>
+    private const float MAX_SIZE = 3f;
+    private const float MIN_SIZE = 1f;
+    public const float MIN_SHIPS = 10f;
+    public const float MAX_SHIPS = 100f;
+
+    public float timeBetweenGrowth;
+    public float ShipCounter;
+
+    void Start()
+    {
+        UpdateLabelAndSize();   
+        StartCoroutine(GrowPlanet());
+    }
+
+    IEnumerator GrowPlanet()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeBetweenGrowth);
+
+            if (ShipCounter < MAX_SHIPS)
+            {
+                ShipCounter++;
+                UpdateLabelAndSize();
+            }
+        }
+    }
+
+    void UpdateLabelAndSize()
+    {
+        TextMesh textLabel = this.GetComponentInChildren<TextMesh>();
+        textLabel.text = ShipCounter.ToString();
+
+        Vector3 currentSize = new Vector3(MAX_SIZE / MAX_SHIPS * ShipCounter, MAX_SIZE / MAX_SHIPS * ShipCounter, 0);
+        this.transform.localScale = currentSize;
+    }
+}
