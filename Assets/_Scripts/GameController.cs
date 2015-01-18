24bf8916cb9053b8a751c074d5e9d6621603fc5e
Ursin <ursin.brunner@drallo.ch>
2015-01-18 14:03:54 +0100

    planets are placed randomely

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,27 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 public class GameController : MonoBehaviour
 {
     public float amountOfPlanets;
     public Vector3 spawnRange;
-    public GameObject planet;
+    public GameObject planetPrototype;
 
+	private List<GameObject> allPlanets = new List<GameObject>();
 
     // Use this for initialization
     void Start()
     {
-        for (int i = 0; i< amountOfPlanets; i++)
-        {
-            SpawnPlanet();
-        }
+
+        CreatePlanets ();
     }
+
+	void CreatePlanets ()
+	{
+		for (int i = 0; i < amountOfPlanets; i++) 
+		{
+			SpawnPlanet ();
+		}
+	}
 
     void SpawnPlanet()
     {
         Vector3 spawnPosition = new Vector3 (Random.Range (-spawnRange.x, spawnRange.x), Random.Range (-spawnRange.y, spawnRange.y), 0);
+		//while ())
+
         Quaternion spawnRotation = Quaternion.identity;
-        GameObject newPlanet = Instantiate (planet, spawnPosition, spawnRotation) as GameObject;
+        GameObject newPlanet = Instantiate (planetPrototype, spawnPosition, spawnRotation) as GameObject;
 
         SetPlanetSizeRandomly(newPlanet);
     }
@@ -29,6 +40,6 @@ public class GameController : MonoBehaviour
     void SetPlanetSizeRandomly(GameObject newPlanet)
     {
         GrowthController growthController = newPlanet.GetComponent<GrowthController>();
-        growthController.ShipCounter = Random.Range(GrowthController.MIN_SHIPS, GrowthController.MAX_SHIPS);
+        growthController.ShipCounter = Mathf.Round(Random.Range(GrowthController.MIN_SHIPS, GrowthController.MAX_SHIPS));
     }
 }
