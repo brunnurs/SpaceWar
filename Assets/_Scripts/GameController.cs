169db9f8394be2663e7fd691edd836f316d10d4a
Ursin <ursin.brunner@drallo.ch>
2015-03-05 19:01:47 +0100

    Avoid overlapping planets

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class GameController : MonoBehaviour {
 
@@ -7,8 +8,12 @@ public class GameController : MonoBehaviour {
 	public int planetCount;
 	public Vector3 borders;
 
+	private List<GameObject> allPlanets;
+
 	void Start () 
 	{
+		allPlanets = new List<GameObject> ();
+
 		CreatePlanets ();
 	}
 
@@ -20,10 +25,32 @@ public class GameController : MonoBehaviour {
 			float yPosition = Random.Range (borders.y * -1, borders.y);
 			
 			Vector3 positionVector = new Vector3 (xPosition, yPosition, -1);
+
+
+			while (IsTooCloseToOtherPlanets(positionVector))
+			{
+				xPosition = Random.Range (borders.x * -1, borders.x);
+				yPosition = Random.Range (borders.y * -1, borders.y);
+
+				positionVector = new Vector3 (xPosition, yPosition, -1);
+			}
 			
 			GameObject newPlanet =	Instantiate (planetPrototype, positionVector, Quaternion.identity) as GameObject;
+			allPlanets.Add(newPlanet);
 		}
 	}
 
 
+	bool IsTooCloseToOtherPlanets (Vector3 randomPosition)
+	{
+		foreach (GameObject planet in allPlanets) 
+		{
+			if(Vector3.Distance(randomPosition,planet.transform.position) < GrowthController1.MAX_SIZE)
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
--- Assets/_Scripts/GrowthController1.cs
+++ Assets/_Scripts/GrowthController1.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 
 public class GrowthController1 : MonoBehaviour {
 
+	public const float MAX_SIZE = 3;
+
 	public int SpaceShipCounter;
 
 	void Start () 
