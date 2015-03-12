248cc84127f33151b7dd0b9db7e4300f896700ec
Ursin <ursin.brunner@drallo.ch>
2015-03-12 19:16:02 +0100

    dummy

---
+++ Assets/_Scripts/FleetLaunchController.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FleetLaunchController : MonoBehaviour {
+
+	GameController gameController;
+
+	void Awake()
+	{
+		gameController = this.GetComponentInParent<GameController>();
+	}
+
+
+	void OnGUI()
+	{
+		if (Event.current.type == EventType.MouseDown) 
+		{
+			GameObject planet = GetPlanetClickedOn();
+
+			if(planet == null)
+			{
+				Debug.Log("No planet hitted!");
+			}
+			else
+			{
+
+				Debug.Log("Planet hitted!");
+			}
+		}
+	}
+
+	GameObject GetPlanetClickedOn ()
+	{
+		Camera currentCamera = Camera.main;
+		Vector3 currentCursorPosition = Input.mousePosition;
+
+		Ray screenRay = currentCamera.ScreenPointToRay (currentCursorPosition);
+
+		RaycastHit hit;
+		Physics.Raycast (screenRay,out hit);
+
+		foreach (GameObject planet in gameController.allPlanets) 
+		{
+			if(hit.collider == planet.GetComponent<Collider>())
+			{
+				return planet;
+			}
+		}
+
+		return null;
+
+	}
+}
---
+++ Assets/_Scripts/PlanetController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetController : MonoBehaviour {
+
+	private Light halo;
+
+	public void Awake()
+	{
+		halo = this.GetComponentInChildren<Light> ();
+	}
+
+	public void Start()
+	{
+		DisableHalo();
+	}
+
+	public void EnableHalo()
+	{
+		halo.enabled = true;
+	}
+
+	public void DisableHalo()
+	{
+		halo.enabled = false;
+	}
+}
